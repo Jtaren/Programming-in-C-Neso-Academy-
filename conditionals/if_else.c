ff1492c700cd796dbcf59848ef30f9787db0a837
jtaren <jatauaren@gmail.com>
2023-12-26 06:13:37 +0100

    if_else.c

---
+++ conditionals/if_else.c
@@ -0,0 +1,22 @@
+// FAQS
+// Is it necessary to put the else part ?
+#include <stdio.h>
+
+/**int main() {
+    int n = 4;
+    if (n == 4)
+        printf("n is 4");
+    else
+        printf("n is not 4");
+}**/
+int main(){
+int n = 4;
+if (n == 4)
+{
+    printf("n is 4");
+}
+else
+{
+    printf("n is not 4");
+}
+}
