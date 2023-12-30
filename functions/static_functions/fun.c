df1acb4d9529623de74ef9e3d79568d96b7977f6
Solomon Jt Aren <78317210+Jtaren@users.noreply.github.com>
2023-12-30 19:18:37 +0100

    Update fun.c

--- functions/static_functions/fun.c
+++ functions/static_functions/fun.c
@@ -1,4 +1,6 @@
-int fun(int a, int b)
+// Static functions are restricted to the files where they are declared
+// Reuse of the same function in another file is possible
+static int fun(int a, int b)
 {
     int c;
     c = a + b;
